e24f9e5c28d164d9e35e0774e05234230d931dea
Rafael Cordones Marcos <rafael.cordones@plexiti.com>
2013-03-11 12:20:38 +0100

    Initial mechanism to locate activities (user tasks, service tasks, gateways, ...) via By selectors like in Selenium

---
+++ src/main/java/org/camunda/bpm/model/fluent/By.java
@@ -0,0 +1,11 @@
+package org.camunda.bpm.model.fluent;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rafa
+ * Date: 3/11/13
+ * Time: 10:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class By {
+}
