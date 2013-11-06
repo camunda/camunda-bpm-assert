880a27709169aa1331e6882d70f3c608d8b08064
jangalinski <jan.galinski@holisticon.de>
2013-11-06 22:19:36 +0100

    introduce example process invoice and sanity tests

---
+++ camunda-bpm-testing-examples/src/test/java/org/camunda/bpm/testing/examples/ReceiveInvoiceProcessTest.java
@@ -0,0 +1,23 @@
+package org.camunda.bpm.testing.examples;
+
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Sanity test to verify the example process is syntactical correct and can be
+ * deployed to the engine. All "real" tests should be implemented in the
+ * according modules (jbehave, fluent, needle, ...).
+ */
+public class ReceiveInvoiceProcessTest {
+
+  @Rule
+  public final ProcessEngineRule processEngineRule = new ProcessEngineRule();
+
+  @Test
+  @Deployment(resources = "camunda-testing-invoice-en.bpmn")
+  public void shouldDeployWithoutErrors() throws Exception {
+    // nothing here, test successful if deployment works
+  }
+}
