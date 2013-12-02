bab291017b4ba6419332aa094e031cddea774d2b
Martin Schimak <martin.schimak@plexiti.com>
2013-12-02 10:52:16 +0100

    Providing an initial set of helper methods in order to allow for fluently writable and business readable process engine tests. (For now just integration tested with an example in camunda-bpm-testing-examples.)

--- camunda-bpm-fest-assertions/src/main/java/org/camunda/bpm/engine/test/fluent/ProcessEngineTests.java
+++ camunda-bpm-fest-assertions/src/main/java/org/camunda/bpm/engine/test/fluent/ProcessEngineTests.java
@@ -4,7 +4,14 @@ import org.camunda.bpm.engine.*;
 import org.camunda.bpm.engine.runtime.ExecutionQuery;
 import org.camunda.bpm.engine.runtime.JobQuery;
 import org.camunda.bpm.engine.runtime.ProcessInstanceQuery;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.task.TaskQuery;
+import org.camunda.bpm.engine.test.fluent.assertions.TaskAssert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.String.format;
 
 /**
  * Convenience class to access all fluent camunda BPM 
@@ -65,6 +72,63 @@ public class ProcessEngineTests extends ProcessEngineAssertions {
 
   public static ExecutionQuery executionQuery() {
     return runtimeService().createExecutionQuery();
+  }
+
+  /**
+   * Helper method to easily construct a map of process variables
+   * @return a map of process variables by passing a list of String -> Object key value pairs.
+   */
+  public static Map<String, Object> withVariables(final String key, final Object value, final Object... keyValuePairs) {
+    if (key == null)
+        throw new IllegalArgumentException(format("Illegal call of withVariables(key = '%s', value = '%s', ...) - key must not be null!", key, value));
+    final Map<String, Object> map = new HashMap<String, Object>();
+    map.put(key, value);
+    if (keyValuePairs != null) {
+      if (keyValuePairs.length % 2 != 0) {
+        throw new IllegalArgumentException(format("Illegal call of withVariables() - must have an even number of arguments, but found length = %s!", keyValuePairs.length + 2));
+      }
+      for (int i = 0; i < keyValuePairs.length; i += 2) {
+        if (!(keyValuePairs[i] instanceof String))
+          throw new IllegalArgumentException(format("Illegal call of withVariables() - keys must be strings, found object of type '%s'!", keyValuePairs[i] != null ? keyValuePairs[i].getClass().getName() : null));
+        map.put((String) keyValuePairs[i], keyValuePairs[i + 1]);
+      }
+    }
+    return map;
+  }
+
+  /**
+   * Helper method to easily access the last asserted task.
+   * @return the last task used as an object under test of a TaskAssert. May return null if no such task exists anymore.
+   */
+  public static Task task() {
+    TaskAssert lastAssert = TaskAssert.lastAssert();
+    if (lastAssert == null)
+        throw new IllegalStateException("Call a task assertion first -  e.g. assertThat(task)... or assertThat(processInstance).task()...!");
+    return taskService().createTaskQuery().taskId(TaskAssert.lastAssert().getActual().getId()).singleResult();
+  }
+
+  /**
+   * Helper method to easily claim a task for a specific assignee.
+   * @return the assigned task - properly refreshed to its assigned state.
+   */
+  public static Task claim(Task task, String assigneeUserId) {
+    taskService().claim(task.getId(), assigneeUserId);
+    return task();
+  }
+
+  /**
+   * Helper method to easily complete a task and pass some process variables. For 
+   * setting those variables, you can use withVariables(String key, Object value, ...)
+   */
+  public static void complete(Task task, Map<String, Object> variables) {
+    taskService().complete(task.getId(), variables);
+  }
+
+  /**
+   * Helper method to easily complete a task.
+   */
+  public static void complete(Task task) {
+    taskService().complete(task.getId());
   }
 
 }
