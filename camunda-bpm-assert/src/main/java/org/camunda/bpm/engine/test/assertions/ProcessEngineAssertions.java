f77c76c308b903c6a9f1b3047e98a7e0ba53eb45
Martin Schimak <martin.schimak@plexiti.com>
2014-01-31 12:54:22 +0100

    Reformatted code of ProcessEngineAssertions class.

--- camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/ProcessEngineAssertions.java
+++ camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/ProcessEngineAssertions.java
@@ -9,95 +9,102 @@ import org.camunda.bpm.engine.task.Task;
 import org.assertj.core.api.Assertions;
 
 /**
- * Class meant to statically access all 
- * camunda BPM Process Engine Assertions. 
- *
+ * Class meant to statically access all
+ * camunda BPM Process Engine Assertions.
+ * <p/>
  * In your code use import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.*;
  *
  * @author Martin Schimak <martin.schimak@plexiti.com>
  */
 public class ProcessEngineAssertions extends Assertions {
 
-    static ThreadLocal<ProcessEngine> processEngine = new ThreadLocal<ProcessEngine>();
+  static ThreadLocal<ProcessEngine> processEngine = new ThreadLocal<ProcessEngine>();
 
-    protected ProcessEngineAssertions() {}
+  protected ProcessEngineAssertions() {
+  }
 
-    /**
-     * Retrieve the processEngine bound to the current testing thread 
-     * via calling init(ProcessEngine processEngine). In case no such
-     * processEngine is bound yet, init(processEngine) is called with
-     * a default process engine.
-     * @throws IllegalStateException in case a processEngine has not 
-     * been initialised yet and cannot be initialised with a default
-     * engine.
-     * @return processEngine bound to the current testing thread
-     */
-    public static ProcessEngine processEngine() {
-        ProcessEngine processEngine = ProcessEngineAssertions.processEngine.get();
-        if (processEngine != null)
-            return processEngine;
-        processEngine = ProcessEngines.getDefaultProcessEngine();
-        if (processEngine != null) {
-            init(processEngine);
-            return processEngine;
-        }
-        throw new IllegalStateException(String.format("Call %s.init(ProcessEngine processEngine) first!", ProcessEngineAssertions.class.getSimpleName()));
+  /**
+   * Retrieve the processEngine bound to the current testing thread
+   * via calling init(ProcessEngine processEngine). In case no such
+   * processEngine is bound yet, init(processEngine) is called with
+   * a default process engine.
+   *
+   * @return processEngine bound to the current testing thread
+   * @throws IllegalStateException in case a processEngine has not
+   *                               been initialised yet and cannot be initialised with a default
+   *                               engine.
+   */
+  public static ProcessEngine processEngine() {
+    ProcessEngine processEngine = ProcessEngineAssertions.processEngine.get();
+    if (processEngine != null)
+      return processEngine;
+    processEngine = ProcessEngines.getDefaultProcessEngine();
+    if (processEngine != null) {
+      init(processEngine);
+      return processEngine;
     }
+    throw new IllegalStateException(String.format("Call %s.init(ProcessEngine processEngine) first!", ProcessEngineAssertions.class.getSimpleName()));
+  }
 
-    /** 
-     * Bind an instance of ProcessEngine to the current testing calls done 
-     * in your test method.
-     * @param processEngine ProcessEngine which should be bound to the 
-     *                      current testing thread.
-     */
-    public static void init(final ProcessEngine processEngine) {
-        ProcessEngineAssertions.processEngine.set(processEngine);
-        AbstractProcessAssert.resetLastAsserts();
-    }
+  /**
+   * Bind an instance of ProcessEngine to the current testing calls done
+   * in your test method.
+   *
+   * @param processEngine ProcessEngine which should be bound to the
+   *                      current testing thread.
+   */
+  public static void init(final ProcessEngine processEngine) {
+    ProcessEngineAssertions.processEngine.set(processEngine);
+    AbstractProcessAssert.resetLastAsserts();
+  }
 
-    /** 
-     * Resets operations done via calling init(ProcessEngine processEngine) 
-     * to its clean state - just as before calling init() for the first time.
-     */
-    public static void reset() {
-        ProcessEngineAssertions.processEngine.remove();
-        AbstractProcessAssert.resetLastAsserts();
-    }
+  /**
+   * Resets operations done via calling init(ProcessEngine processEngine)
+   * to its clean state - just as before calling init() for the first time.
+   */
+  public static void reset() {
+    ProcessEngineAssertions.processEngine.remove();
+    AbstractProcessAssert.resetLastAsserts();
+  }
 
-    /**
-     * Assert that... the given ProcessDefinition meets your expecations.
-     * @param actual ProcessDefinition under test
-     * @return Assert object offering ProcessDefinition specific assertions.
-     */
-    public static ProcessDefinitionAssert assertThat(final ProcessDefinition actual) {
-        return ProcessDefinitionAssert.assertThat(processEngine(), actual);
-    }
+  /**
+   * Assert that... the given ProcessDefinition meets your expecations.
+   *
+   * @param actual ProcessDefinition under test
+   * @return Assert object offering ProcessDefinition specific assertions.
+   */
+  public static ProcessDefinitionAssert assertThat(final ProcessDefinition actual) {
+    return ProcessDefinitionAssert.assertThat(processEngine(), actual);
+  }
 
-    /**
-     * Assert that... the given ProcessInstance meets your expecations.
-     * @param actual ProcessInstance under test
-     * @return Assert object offering ProcessInstance specific assertions.
-     */
-    public static ProcessInstanceAssert assertThat(final ProcessInstance actual) {
-        return ProcessInstanceAssert.assertThat(processEngine(), actual);
-    }
+  /**
+   * Assert that... the given ProcessInstance meets your expecations.
+   *
+   * @param actual ProcessInstance under test
+   * @return Assert object offering ProcessInstance specific assertions.
+   */
+  public static ProcessInstanceAssert assertThat(final ProcessInstance actual) {
+    return ProcessInstanceAssert.assertThat(processEngine(), actual);
+  }
 
-    /**
-     * Assert that... the given Task meets your expecations.
-     * @param actual Task under test
-     * @return Assert object offering Task specific assertions.
-     */
-    public static TaskAssert assertThat(final Task actual) {
-        return TaskAssert.assertThat(processEngine(), actual);
-    }
+  /**
+   * Assert that... the given Task meets your expecations.
+   *
+   * @param actual Task under test
+   * @return Assert object offering Task specific assertions.
+   */
+  public static TaskAssert assertThat(final Task actual) {
+    return TaskAssert.assertThat(processEngine(), actual);
+  }
 
-    /**
-     * Assert that... the given Job meets your expecations.
-     * @param actual Job under test
-     * @return Assert object offering Job specific assertions.
-     */
-    public static JobAssert assertThat(final Job actual) {
-        return JobAssert.assertThat(processEngine(), actual);
-    }
+  /**
+   * Assert that... the given Job meets your expecations.
+   *
+   * @param actual Job under test
+   * @return Assert object offering Job specific assertions.
+   */
+  public static JobAssert assertThat(final Job actual) {
+    return JobAssert.assertThat(processEngine(), actual);
+  }
 
 }
