24ddd146f9d7d5e350d7de5b50f35b47f07f99f3
Simon Zambrovski <simon.zambrovski@holisticon.de>
2015-05-23 10:30:49 +0200

    feat(cmmn-assertions): add initial assert types
    (cherry picked from commit 8aebbb1)

--- camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/ProcessEngineAssertions.java
+++ camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/ProcessEngineAssertions.java
@@ -1,11 +1,12 @@
 package org.camunda.bpm.engine.test.assertions;
 
 import org.camunda.bpm.engine.*;
+import org.camunda.bpm.engine.repository.CaseDefinition;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
+import org.camunda.bpm.engine.runtime.CaseInstance;
 import org.camunda.bpm.engine.runtime.Job;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
-
 import org.assertj.core.api.Assertions;
 import org.camunda.bpm.engine.test.util.CamundaBpmApi;
 
@@ -92,7 +93,7 @@ public class ProcessEngineAssertions extends Assertions {
   }
 
   /**
-   * Assert that... the given ProcessDefinition meets your expecations.
+   * Assert that... the given ProcessDefinition meets your expectations.
    *
    * @param   actual ProcessDefinition under test
    * @return  Assert object offering ProcessDefinition specific assertions.
@@ -102,7 +103,7 @@ public class ProcessEngineAssertions extends Assertions {
   }
 
   /**
-   * Assert that... the given ProcessInstance meets your expecations.
+   * Assert that... the given ProcessInstance meets your expectations.
    *
    * @param   actual ProcessInstance under test
    * @return  Assert object offering ProcessInstance specific assertions.
@@ -112,7 +113,7 @@ public class ProcessEngineAssertions extends Assertions {
   }
 
   /**
-   * Assert that... the given Task meets your expecations.
+   * Assert that... the given Task meets your expectations.
    *
    * @param   actual Task under test
    * @return  Assert object offering Task specific assertions.
@@ -122,13 +123,33 @@ public class ProcessEngineAssertions extends Assertions {
   }
 
   /**
-   * Assert that... the given Job meets your expecations.
+   * Assert that... the given Job meets your expectations.
    *
    * @param   actual Job under test
    * @return  Assert object offering Job specific assertions.
    */
   public static JobAssert assertThat(final Job actual) {
     return JobAssert.assertThat(processEngine(), actual);
+  }
+
+  /**
+   * Assert that... the given CaseInstance meets your expectations.
+   *
+   * @param   actual CaseInstance under test
+   * @return  Assert object offering CaseInstance specific assertions.
+   */
+  public static CaseInstanceAssert assertThat(final CaseInstance actual) {
+    return CaseInstanceAssert.assertThat(processEngine(), actual);
+  }
+
+  /**
+   * Assert that... the given CaseDefinition meets your expectations.
+   *
+   * @param   actual ProcessDefinition under test
+   * @return  Assert object offering ProcessDefinition specific assertions.
+   */
+  public static CaseDefinitionAssert assertThat(final CaseDefinition actual) {
+    return CaseDefinitionAssert.assertThat(processEngine(), actual);
   }
 
 }
