63be1b8c0c49f6e6eeb252bb9bb91dc4b54aca70
Malte Sörensen <malte.soerensen@holisticon.de>
2015-07-16 19:15:58 +0200

    feat(cmmn-assertions): add assertions for cmmn tasks and tests
    - complete (CMMN Model)
    - complete (task.isActive, .isCompleted)
    - complete (case.isActive, .isCompleted)
    
    (cherry picked from commit 9ac577c)

--- camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/cmmn/CaseInstanceAssert.java
+++ camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/cmmn/CaseInstanceAssert.java
@@ -2,40 +2,57 @@ package org.camunda.bpm.engine.test.assertions.cmmn;
 
 import org.assertj.core.api.Assertions;
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.history.HistoricActivityInstance;
+import org.camunda.bpm.engine.history.HistoricCaseActivityInstance;
+import org.camunda.bpm.engine.history.HistoricCaseActivityInstanceQuery;
 import org.camunda.bpm.engine.impl.cmmn.execution.CaseExecutionState;
 import org.camunda.bpm.engine.impl.cmmn.execution.CmmnActivityExecution;
 import org.camunda.bpm.engine.runtime.CaseExecution;
+import org.camunda.bpm.engine.runtime.CaseExecutionQuery;
 import org.camunda.bpm.engine.runtime.CaseInstance;
 import org.camunda.bpm.engine.test.assertions.AbstractProcessAssert;
 
+/**
+ * @author Malte Sörensen <malte.soerensen@holisticon.de>
+ * @author Martin Günther <martin.guenter@holisticon.de>
+ */
 public class CaseInstanceAssert extends AbstractProcessAssert<CaseInstanceAssert, CaseInstance> {
-
-  public static CaseInstanceAssert assertThat(final ProcessEngine engine, final CaseInstance actual) {
-    return new CaseInstanceAssert(engine, actual);
-  }
 
   protected CaseInstanceAssert(final ProcessEngine engine, final CaseInstance actual) {
     super(engine, actual, CaseInstanceAssert.class);
   }
 
+  public static CaseInstanceAssert assertThat(final ProcessEngine engine, final CaseInstance actual) {
+    return new CaseInstanceAssert(engine, actual);
+  }
+
   public CaseInstanceAssert isActive() {
-    CmmnActivityExecution caseExecution = (CmmnActivityExecution) caseService()
-      .createCaseExecutionQuery()
-      .caseExecutionId(getCurrent().getId())
-      .singleResult();
+    assertInState(CaseExecutionState.ACTIVE);
+    return this;
+  }
 
-    CaseExecutionState currentState = caseExecution.getCurrentState();
-
-    Assertions.assertThat(currentState)
-      .overridingErrorMessage("Expected case instance '"+toString(getActual()) + "' to be active, but it is '"+currentState+"'")
-      .isEqualTo(CaseExecutionState.ACTIVE);
+  public CaseInstanceAssert isCompleted() {
+    assertInState(CaseExecutionState.COMPLETED);
     return this;
   }
 
   public CaseTaskAssert task(String activityId) {
-    CaseExecution caseExecution = caseService().createCaseExecutionQuery().caseInstanceId(getActual().getCaseInstanceId()).activityId(activityId).singleResult();
-    Assertions.assertThat(caseExecution).overridingErrorMessage("Task '"+activityId+"' not found!").isNotNull();
-    return new CaseTaskAssert(engine, caseExecution);
+    HistoricCaseActivityInstance activityInstance = historicCaseActivityInstanceQuery().caseActivityId(activityId).singleResult();
+    Assertions.assertThat(activityInstance).overridingErrorMessage("Task '" + activityId + "' not found!").isNotNull();
+    return new CaseTaskAssert(engine, activityInstance);
+  }
+
+  private void assertInState(CaseExecutionState expectedState) {
+    CmmnActivityExecution caseExecution = (CmmnActivityExecution) caseExecutionQuery().caseExecutionId(getActual().getId()).singleResult();
+    CaseExecutionState currentState = caseExecution.getCurrentState();
+    Assertions.assertThat(currentState)
+      .overridingErrorMessage("Expected case instance " + toString(getActual()) + " to be " + expectedState + ", but it is " + currentState)
+      .isEqualTo(expectedState);
+  }
+
+  @Override
+  protected CaseExecutionQuery caseExecutionQuery() {
+    return super.caseExecutionQuery().caseInstanceId(getActual().getCaseInstanceId());
   }
 
   @Override
@@ -43,6 +60,9 @@ public class CaseInstanceAssert extends AbstractProcessAssert<CaseInstanceAssert
     return caseInstanceQuery().singleResult();
   }
 
+  private HistoricCaseActivityInstanceQuery historicCaseActivityInstanceQuery() {
+    return historyService().createHistoricCaseActivityInstanceQuery().caseInstanceId(getActual().getCaseInstanceId());
+  }
 
   @Override
   protected String toString(CaseInstance caseInstance) {
