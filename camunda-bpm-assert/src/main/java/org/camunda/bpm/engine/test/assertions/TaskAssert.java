bf047f9f04509c45cf8220745774d733d458baa4
Martin Schimak <martin.schimak@plexiti.com>
2015-08-31 15:05:45 +0200

    refactor(TaskAssert): implement hasFormKey() with formService, hence without need for v7.2.0

--- camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/TaskAssert.java
+++ camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/TaskAssert.java
@@ -145,36 +145,18 @@ public class TaskAssert extends AbstractProcessAssert<TaskAssert, Task> {
   /**
    * Verifies the expectation that the {@link Task} has a specified form key.
    * 
-   * @param formKey
-   *          the expected form key.
-   * @return this {@link TaskAsert}
+   * @param   formKey the expected form key.
+   * @return  this {@link TaskAssert}
    */
   public TaskAssert hasFormKey(String formKey) {
-    Task current = getInitializedExistingCurrent();
-    Assertions.assertThat(current.getFormKey())
-        .overridingErrorMessage("Expecting %s to have a form key %s, but found it to to have form key '%s'!", toString(current), formKey, current.getFormKey())
-        .isEqualTo(formKey);
+    Task current = getExistingCurrent();
+    String actualformKey = formService().getTaskFormKey(current.getProcessDefinitionId(), current.getTaskDefinitionKey());
+    Assertions.assertThat(actualformKey)
+      .overridingErrorMessage("Expecting %s to have a form key '%s', but found it to to have form key '%s'!", toString(current), formKey, actualformKey)
+      .isEqualTo(formKey);
     return this;
   }
-
-  protected Task getInitializedExistingCurrent() {
-    Assertions.assertThat(actual)
-      .overridingErrorMessage("Expecting assertion to be called on non-null actual, but found it to be null!")
-      .isNotNull();
-    Task current = getInitializedCurrent();
-    Assertions.assertThat(current)
-      .overridingErrorMessage(
-        "Expecting assertion to be called on non-null current state of actual %s, " +
-        "but found it to be null!",
-        actual)
-      .isNotNull();
-    return current;
-  }
-  protected Task getInitializedCurrent() {
-    return taskQuery().taskId(actual.getId()).initializeFormKeys().singleResult();
-  }
-
-
+  
   /**
    * Verifies the internal id of a {@link Task}.
    * 
