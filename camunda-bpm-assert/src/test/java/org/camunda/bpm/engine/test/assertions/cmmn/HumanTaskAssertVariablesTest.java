5dc89b77666f3bea92244269d1b23b37d98df87f
Martin Günther <martin.guenther@holisticon.de>
2016-03-15 15:25:49 +0100

    test: #67 add tests for variables() support for HumanTaskAssert

--- camunda-bpm-assert/src/test/java/org/camunda/bpm/engine/test/assertions/cmmn/HumanTaskAssertVariablesTest.java
+++ camunda-bpm-assert/src/test/java/org/camunda/bpm/engine/test/assertions/cmmn/HumanTaskAssertVariablesTest.java
@@ -24,33 +24,54 @@ public class HumanTaskAssertVariablesTest extends ProcessAssertTestCase {
   @Deployment(resources = { "cmmn/HumanTaskAssert-variables.cmmn" })
   public void testVariables_Success() {
     // Given
-    final CaseInstance caseInstance = caseService().createCaseInstanceByKey(CASE_KEY);
-    setAVariableOnHumanTask();
+    final CaseInstance caseInstance = createCaseInstance();
 
     // When
+    setVariablesOnHumanTask();
+
     // Then
     assertThat(caseInstance).humanTask(TASK_A).variables().containsEntry("aVariable", "aValue");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().containsEntry("bVariable", "bValue");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().containsKey("aVariable");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().containsKey("bVariable");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().containsKeys("aVariable", "bVariable");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().containsValue("aValue");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().doesNotContainValue("cValue");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().doesNotContainEntry("cVariable", "cValue");
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().isNotEmpty();
   }
 
   @Test
   @Deployment(resources = { "cmmn/HumanTaskAssert-variables.cmmn" })
   public void testVariables_Success_No_Variables() {
     // Given
-    final CaseInstance caseInstance = caseService().createCaseInstanceByKey(CASE_KEY);
+    final CaseInstance caseInstance = createCaseInstance();
 
     // When
+
     // Then
     assertThat(caseInstance).humanTask(TASK_A).variables().isEmpty();
+    // And
+    assertThat(caseInstance).humanTask(TASK_A).variables().doesNotContainEntry("aVariable", "aValue");
   }
 
   @Test
   @Deployment(resources = { "cmmn/HumanTaskAssert-variables.cmmn" })
   public void testVariables_Failure_HumanTask_Completed() {
     // Given
-    final CaseInstance caseInstance = caseService().createCaseInstanceByKey(CASE_KEY);
-    setAVariableOnHumanTaskAndComplete();
+    final CaseInstance caseInstance = createCaseInstance();
 
     // When
+    setAVariableOnHumanTaskAndCompleteHumanTask();
+
     // Then
     expect(new Failure() {
       @Override
@@ -60,17 +81,20 @@ public class HumanTaskAssertVariablesTest extends ProcessAssertTestCase {
     });
   }
 
-  private void setAVariableOnHumanTaskAndComplete() {
-    getCommandExectuionToSetAVariableOnAHumanTask().complete();
+  private CaseInstance createCaseInstance() {
+    return caseService().createCaseInstanceByKey(CASE_KEY);
   }
 
-  private void setAVariableOnHumanTask() {
-    getCommandExectuionToSetAVariableOnAHumanTask().execute();
+  private void setAVariableOnHumanTaskAndCompleteHumanTask() {
+    caseExecution(TASK_A).setVariable("aVariable", "aValue").complete();
   }
 
-  private CaseExecutionCommandBuilder getCommandExectuionToSetAVariableOnAHumanTask() {
-    return caseService().withCaseExecution(caseService().createCaseExecutionQuery().activityId(TASK_A).singleResult().getId()).setVariable(
-        "aVariable",
-        "aValue");
+  private void setVariablesOnHumanTask() {
+    caseExecution(TASK_A).setVariable("aVariable", "aValue").setVariable("bVariable", "bValue").execute();
   }
+
+  private CaseExecutionCommandBuilder caseExecution(String activityId) {
+    return caseService().withCaseExecution(caseService().createCaseExecutionQuery().activityId(activityId).singleResult().getId());
+  }
+
 }
