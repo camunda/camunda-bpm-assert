646429d6169a20a539c44d444e9534de8b5676c2
Martin Günther <martin.guenther@holisticon.de>
2016-03-31 13:22:46 +0200

    docs: #76 add Javadoc to CaseTaskAssert

--- camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/cmmn/CaseTaskAssert.java
+++ camunda-bpm-assert/src/main/java/org/camunda/bpm/engine/test/assertions/cmmn/CaseTaskAssert.java
@@ -1,59 +1,101 @@
 package org.camunda.bpm.engine.test.assertions.cmmn;
 
 import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.impl.cmmn.execution.CaseExecutionState;
 import org.camunda.bpm.engine.runtime.CaseExecution;
+import org.camunda.bpm.model.cmmn.instance.CaseTask;
 
 /**
  * @author Martin Schimak <martin.schimak@plexiti.com>
  */
 public class CaseTaskAssert extends AbstractCaseAssert<CaseTaskAssert, CaseExecution> {
 
-	protected CaseTaskAssert(final ProcessEngine engine, final CaseExecution actual) {
-		super(engine, actual, CaseTaskAssert.class);
-	}
+  protected CaseTaskAssert(final ProcessEngine engine, final CaseExecution actual) {
+    super(engine, actual, CaseTaskAssert.class);
+  }
 
-	protected static CaseTaskAssert assertThat(final ProcessEngine engine, final CaseExecution actual) {
-		return new CaseTaskAssert(engine, actual);
-	}
+  protected static CaseTaskAssert assertThat(final ProcessEngine engine, final CaseExecution actual) {
+    return new CaseTaskAssert(engine, actual);
+  }
 
-	@Override
-	public CaseTaskAssert isAvailable() {
-		return super.isAvailable();
-	}
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'available'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isAvailable() {
+    return super.isAvailable();
+  }
 
-	@Override
-	public CaseTaskAssert isEnabled() {
-		return super.isEnabled();
-	}
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'enabled'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isEnabled() {
+    return super.isEnabled();
+  }
 
-	@Override
-	public CaseTaskAssert isDisabled() {
-		return super.isDisabled();
-	}
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'disabled'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isDisabled() {
+    return super.isDisabled();
+  }
 
-	@Override
-	public CaseTaskAssert isActive() {
-		return super.isActive();
-	}
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'active'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isActive() {
+    return super.isActive();
+  }
 
-	@Override
-	public CaseTaskAssert isSuspended() {
-		return super.isSuspended();
-	}
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'suspended'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isSuspended() {
+    return super.isSuspended();
+  }
 
-	@Override
-	public CaseTaskAssert isCompleted() {
-		return super.isCompleted();
-	}
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'completed'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isCompleted() {
+    return super.isCompleted();
+  }
 
-	@Override
-	public CaseTaskAssert isFailed() {
-		return super.isFailed();
-	}
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'failed'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isFailed() {
+    return super.isFailed();
+  }
 
-	@Override
-	public CaseTaskAssert isTerminated() {
-		return super.isTerminated();
-	}
-	
+  /**
+   * Verifies the expectation that the {@link CaseTask} is in {@link CaseExecutionState} 'terminated'.
+   * 
+   * @return this {@link CaseTaskAssert}
+   */
+  @Override
+  public CaseTaskAssert isTerminated() {
+    return super.isTerminated();
+  }
+
 }
