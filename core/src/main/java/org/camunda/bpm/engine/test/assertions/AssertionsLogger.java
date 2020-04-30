6821a150f2907ad030cc6bc69cdb7685432fb3a1
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2020-04-30 12:16:11 +0200

    clarify log messages

--- core/src/main/java/org/camunda/bpm/engine/test/assertions/AssertionsLogger.java
+++ core/src/main/java/org/camunda/bpm/engine/test/assertions/AssertionsLogger.java
@@ -30,13 +30,13 @@ public class AssertionsLogger extends BaseLogger {
       AssertionsLogger.class, "BPM_ASSERT", "org.camunda.bpm.engine.test.assertions", "00");
   
   public void collectTransitionInstances(ActivityInstance activityInstance) {
-    logDebug("001", "collecting transition instances for activity instance '{}' with ID '{}'", 
-        activityInstance.getActivityName(),
-        activityInstance.getActivityId());
+    logDebug("001", "collecting transition instances for activity instance '{}' with Name '{}'", 
+        activityInstance.getActivityId(),
+        activityInstance.getActivityName());
   }
   
   public void foundTransitionInstances(TransitionInstance transitionInstance) {
-    logDebug("002", "found transition instance '{}'", transitionInstance.getActivityName());
+    logDebug("002", "found transition instance '{}'", transitionInstance.getActivityId());
   }
 
 }
