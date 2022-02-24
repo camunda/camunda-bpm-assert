67295e63c829ea07600d8a376b6844bf3ffec069
Daniel Kelemen <daniel.kelemen@camunda.com>
2022-02-24 13:30:32 +0100

    chore(project): jdk17 support
    - remove powermock
    - update mockito to latest version & add mockito-inline for static tests
    - fix ProcessEngineTestsTest & CmmnAwareTestsTest
    - add jdk17 CI target
    
    related to CAM-14399

--- examples/bpmn/src/test/java/org/camunda/bpm/engine/test/assertions/examples/JobAnnouncementPublicationProcessTest.java
+++ examples/bpmn/src/test/java/org/camunda/bpm/engine/test/assertions/examples/JobAnnouncementPublicationProcessTest.java
@@ -30,7 +30,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
 /**
